67143a2a2cc2f97ddce027de4733e4f66252e826
Dwergi <sebastian.nordgren@gmail.com>
2015-07-02 12:54:30 +0300

    Removed Eigen, cleaned up project.

--- src/PrecompiledHeader.h
+++ src/PrecompiledHeader.h
@@ -12,32 +12,4 @@ typedef unsigned int uint;
 #include "AutoList.h"
 #include "TypeInfo.h"
 #include "Member.h"
-
-//#define USE_EIGEN
-
-#ifdef USE_EIGEN
-#define EIGEN_MATRIXBASE_PLUGIN "EigenExtensions.h"
-
-#include "Eigen/Eigen"
-#include "Eigen/StdVector"
-
-typedef Eigen::Vector2f Vector2f;
-typedef Eigen::Vector2d Vector2d;
-typedef Eigen::Vector3f Vector3f;
-typedef Eigen::Vector3d Vector3d;
-typedef Eigen::Vector4f Vector4f;
-typedef Eigen::Vector4d Vector4d;
-
-typedef Eigen::Matrix3f Matrix3;
-typedef Eigen::Matrix4f Matrix4;
-
-template< typename T >
-class aligned_vector
-	: public std::vector< T, Eigen::aligned_allocator< T > >
-{
-
-};
-
-#else
 #include "Vector.h"
-#endif
