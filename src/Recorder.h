0fe413f634793dc268be9ef62c3e760b0aa2ea1b
unknown <sebastiann@sebastian.rmd.remedy.fi>
2015-10-11 16:16:56 +0300

    Recorder split into .inl file.

--- src/Recorder.h
+++ src/Recorder.h
@@ -28,67 +28,22 @@ namespace dd
 
 	public:
 
-		Recorder( dd::Property* prop )
-			: m_current( (T*) prop->GetPtr() )
-		{
-		}
+		Recorder( dd::Property* prop );
+		Recorder( T& value, int size = HISTORY_SIZE );
+		
+		Recorder<T>& operator=( const T& newValue );
 
-		Recorder( T& value, int size = HISTORY_SIZE )
-			: m_current( &value )
-		{
-			m_undo.Resize( HISTORY_SIZE );
-		}
+		void Undo();
 
-		Recorder<T>& operator=( const T& newValue )
-		{
-			if( m_undo.Size() == HISTORY_SIZE )
-			{
-				m_undo.RemoveOrdered( 0 );
-			}
+		operator T();
+		operator T() const;
 
-			m_undo.Add( *m_current );
+		T& Get();
+		const T& Get() const;
 
-			*m_current = newValue;
+		bool operator==( const T& other ) const;
 
-			return *this;
-		}
-
-		void Undo()
-		{
-			ASSERT( m_undo.Size() > 0 );
-
-			*m_current = m_undo.Pop();
-		}
-
-		operator T()
-		{
-			return *m_current;
-		}
-
-		operator T() const
-		{
-			return *m_current;
-		}
-
-		T& Get()
-		{
-			return *m_current;
-		}
-
-		const T& Get() const
-		{
-			return *m_current;
-		}
-
-		bool operator==( const T& other ) const
-		{
-			return *m_current == other;
-		}
-
-		uint GetUndoHistorySize() const
-		{
-			return (uint) m_undo.Size();
-		}
+		uint GetUndoHistorySize() const;
 	};
 	//---------------------------------------------------------------------------
 
@@ -99,48 +54,18 @@ namespace dd
 	protected:
 
 		Vector<T> m_redo;
-
 		typedef Recorder<T> base;
 
 	public:
 
-		FullRecorder( T& value, int history_size = Recorder<T>::HISTORY_SIZE )
-			: base( value, history_size )
-		{
-			m_redo.Resize( history_size );
-		}
+		FullRecorder( T& value, int history_size = Recorder<T>::HISTORY_SIZE );
 
-		FullRecorder<T>& operator=( const T& newValue )
-		{
-			m_redo.Clear();
+		FullRecorder<T>& operator=( const T& newValue );
 
-			base::operator=( newValue );
-
-			return *this;
-		}
-
-		void Undo()
-		{
-			if( m_redo.Size() == HISTORY_SIZE )
-			{
-				m_redo.RemoveOrdered( 0 );
-			}
-
-			m_redo.Add( *m_current );
-
-			base::Undo();
-		}
-
-		void Redo()
-		{
-			ASSERT( m_redo.Size() > 0 );
-
-			base::operator=( m_redo.Pop() );
-		}
-
-		uint GetRedoHistorySize() const
-		{
-			return (uint) m_redo.Size();
-		}
+		void Undo();
+		void Redo();
+		uint GetRedoHistorySize() const;
 	};
 }
+
+#include "Recorder.inl"
---
+++ src/Recorder.inl
@@ -0,0 +1,119 @@
+namespace dd
+{
+	template <typename T>
+	Recorder<T>::Recorder( Property* prop )
+		: m_current( (T*) prop->GetPtr() )
+	{
+	}
+
+	template <typename T>
+	Recorder<T>::Recorder( T& value, int size = HISTORY_SIZE )
+		: m_current( &value )
+	{
+		m_undo.Resize( HISTORY_SIZE );
+	}
+
+	template <typename T>
+	Recorder<T>& Recorder<T>::operator=( const T& newValue )
+	{
+		if( m_undo.Size() == HISTORY_SIZE )
+		{
+			m_undo.RemoveOrdered( 0 );
+		}
+
+		m_undo.Add( *m_current );
+
+		*m_current = newValue;
+
+		return *this;
+	}
+
+	template <typename T>
+	void Recorder<T>::Undo()
+	{
+		ASSERT( m_undo.Size() > 0 );
+
+		*m_current = m_undo.Pop();
+	}
+
+	template <typename T>
+	Recorder<T>::operator T()
+	{
+		return *m_current;
+	}
+
+	template <typename T>
+	Recorder<T>::operator T() const
+	{
+		return *m_current;
+	}
+
+	template <typename T>
+	T& Recorder<T>::Get()
+	{
+		return *m_current;
+	}
+
+	template <typename T>
+	const T& Recorder<T>::Get() const
+	{
+		return *m_current;
+	}
+
+	template <typename T>
+	bool Recorder<T>::operator==( const T& other ) const
+	{
+		return *m_current == other;
+	}
+
+	template <typename T>
+	uint Recorder<T>::GetUndoHistorySize() const
+	{
+		return m_undo.Size();
+	}
+	//---------------------------------------------------------------------------
+
+	template <typename T>
+	FullRecorder<T>::FullRecorder( T& value, int history_size = Recorder<T>::HISTORY_SIZE )
+		: base( value, history_size )
+	{
+		m_redo.Resize( history_size );
+	}
+
+	template <typename T>
+	FullRecorder<T>& FullRecorder<T>::operator=( const T& newValue )
+	{
+		m_redo.Clear();
+
+		base::operator=( newValue );
+
+		return *this;
+	}
+
+	template <typename T>
+	void FullRecorder<T>::Undo()
+	{
+		if( m_redo.Size() == HISTORY_SIZE )
+		{
+			m_redo.RemoveOrdered( 0 );
+		}
+
+		m_redo.Add( *m_current );
+
+		base::Undo();
+	}
+
+	template <typename T>
+	void FullRecorder<T>::Redo()
+	{
+		ASSERT( m_redo.Size() > 0 );
+
+		base::operator=( m_redo.Pop() );
+	}
+
+	template <typename T>
+	uint FullRecorder<T>::GetRedoHistorySize() const
+	{
+		return m_redo.Size();
+	}
+}
