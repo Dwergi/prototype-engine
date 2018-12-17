01386046772212ecdac5d7c046fb9a4e00d1a0cd
Sebastian Nordgren <sebastian.nordgren@gmail.com>
2018-12-17 20:52:45 +0200

    Missed file.

---
+++ src/RenderCommand.cpp
@@ -0,0 +1,17 @@
+//
+// RenderCommand.cpp
+// Copyright (C) Sebastian Nordgren 
+// December 13th 2018
+//
+
+#include "PCH.h"
+#include "RenderCommand.h"
+
+namespace ddr
+{
+	RenderCommand::RenderCommand( CommandType type ) :
+		Type( type )
+	{
+
+	}
+}
